10c8e1487c8d0ed49630a6a4cc336ad837beef4f
Sangjoon Bob Lee <bobleesj@gmail.com>
2024-11-08 22:31:06 -0500

    refactor pytest

--- tests/test_NMF_analysis_code.py
+++ tests/test_NMF_analysis_code.py
@@ -1,62 +1,61 @@
-import os
-import tempfile
+import shutil
+from os.path import exists, join
 from pathlib import Path
-from shutil import rmtree
 
 import pytest
 
 from diffpy.nmf_mapping.main import main
 
 dir = Path(__file__).parent.resolve()
-
-data_dir = os.path.join(dir, "data/synthetic_r_vs_gr")
-
-test_map = [
-    ([data_dir, "--xrange", "5,10"], "output_1", "Number of components: 3\n"),
-    ([data_dir], "output_2", "Number of components: 3\n"),
-    ([data_dir, "--xrange", "5,10", "12,15"], "output_3", "Number of components: 3\n"),
-]
+data_dir = join(dir, "data/synthetic_r_vs_gr")
 
 
-@pytest.fixture(scope="session")
-def temp_dir():
-    """A test fixture that creates and destroys tes outputs in a temporary
-    directory.
-    This will yield the path to the directory.
-    """
-    cwd = os.getcwd()
-    name = "outputs"
-    temp_dir = Path(tempfile.gettempdir())
-    repo = os.path.join(temp_dir, name)
-    if os.path.exists(repo):
-        rmtree(repo)
-    os.chdir(temp_dir)
-    os.mkdir(name)
-    os.chdir(cwd)
-    yield repo
-    os.chdir(cwd)
-    rmtree(repo)
+@pytest.mark.parametrize(
+    "args, output_dir",
+    [
+        (["tests/data/synthetic_r_vs_gr", "--xrange", "5,10", "--show", "false"], "output_1"),
+        (["tests/data/synthetic_r_vs_gr", "--show", "false"], "output_2"),
+        (["tests/data/synthetic_r_vs_gr", "--xrange", "5,10", "12,15", "--show", "false"], "output_3"),
+    ],
+)
+def test_nmf_mapping_code(args, output_dir, tmpdir):
 
+    # Save the result in ("nmf_result") at the top project level (default behavior)
+    main(args=args)
 
-@pytest.mark.parametrize("tm", test_map)
-def test_nmf_mapping_code(tm, temp_dir, capsys):
-    data_dir = tm[0]
-    working_dir = Path(temp_dir)
-    os.chdir(working_dir)
-    main(args=data_dir)
-    out, err = capsys.readouterr()
-    assert out == tm[2]
-    results_dir = os.path.join(working_dir, "nmf_result")
-    os.chdir(results_dir)
-    expected_base = os.path.join(os.path.dirname(__file__), "output")
-    test_specific_dir = os.path.join(expected_base, tm[1])
-    for root, dirs, files in os.walk("."):
-        for file in files:
-            if file in os.listdir(test_specific_dir):
-                fn1 = os.path.join(results_dir, file)
-                with open(fn1, "r") as f:
-                    actual = f.read()
-                fn2 = os.path.join(test_specific_dir, file)
-                with open(fn2, "r") as f:
-                    expected = f.read()
-                assert expected == actual
+    # Define the copied results directory in tmpdir
+    tmp_results_dir = join(tmpdir, "nmf_result")
+    expected_output_dir = join("tests/output", output_dir)
+
+    # Copy the output to tmpdir
+    shutil.copytree("nmf_result", tmp_results_dir)
+
+    # Remove the nmf_result folder from the top project level
+    shutil.rmtree("nmf_result")
+
+    # Define the specific JSON files to check
+    json_files_to_check = [
+        "component_index_vs_pratio_col.json",
+        "component_index_vs_RE_value.json",
+        "x_index_vs_y_col_components.json",
+    ]
+
+    # Compare each specified .json file
+    for json_file in json_files_to_check:
+        # Define paths for actual and expected .json files
+        actual_file_path = join(tmp_results_dir, json_file)
+        expected_file_path = join(expected_output_dir, json_file)
+
+        # Ensure the file exists in both locations
+        assert exists(actual_file_path)
+        assert exists(expected_file_path)
+
+        # Read and compare file contents
+        with open(actual_file_path, "r") as actual_file:
+            actual = actual_file.read()
+
+        with open(expected_file_path, "r") as expected_file:
+            expected = expected_file.read()
+
+        # Assert that the contents match
+        assert actual == expected
