a20d3121c9b8c0386a7201d7a1bb374dbb058531
Dasun Abeykoon <Dasun20202020@gmail.com>
2025-06-27 12:16:20 -0700

    skpkg: apply black to all files in the project dir

--- src/diffpy/version.py
+++ src/diffpy/version.py
@@ -13,8 +13,7 @@
 #
 ##############################################################################
 
-"""Definition of __version__ and __date__ for diffpy.nmf_mapping.
-"""
+"""Definition of __version__ and __date__ for diffpy.nmf_mapping."""
 
 __id__ = "$Id$"
 
@@ -24,6 +23,8 @@ from pkg_resources import get_distribution
 __version__ = get_distribution("diffpy.nmf_mapping").version
 
 # we assume that tag_date was used and __version__ ends in YYYYMMDD
-__date__ = __version__[-8:-4] + "-" + __version__[-4:-2] + "-" + __version__[-2:]
+__date__ = (
+    __version__[-8:-4] + "-" + __version__[-4:-2] + "-" + __version__[-2:]
+)
 
 # End of file
--- tests/test_load_data.py
+++ tests/test_load_data.py
@@ -13,9 +13,19 @@ def test_load_data():
     bfto_dir = os.path.join(dir, "data/synthetic_r_vs_gr")
     bfto_expected_filenames = np.array([f"synthetic{i}" for i in range(50)])
     loaded_filenames = nmf.load_data(bfto_dir)[1]
-    assert np.testing.assert_array_equal(loaded_filenames, bfto_expected_filenames) is None
+    assert (
+        np.testing.assert_array_equal(
+            loaded_filenames, bfto_expected_filenames
+        )
+        is None
+    )
 
     # make sure interpolation to same r-grid is working
     diff_r_grid_dir = os.path.join(dir, "data", "different_r_grid")
     diff_r_grid_arr = nmf.load_data(diff_r_grid_dir)[0]
-    assert np.testing.assert_array_equal(diff_r_grid_arr[0][:, 0], diff_r_grid_arr[1][:, 0]) is None
+    assert (
+        np.testing.assert_array_equal(
+            diff_r_grid_arr[0][:, 0], diff_r_grid_arr[1][:, 0]
+        )
+        is None
+    )
